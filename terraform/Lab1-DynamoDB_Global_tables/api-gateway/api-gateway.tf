936aea77ab0e62001e9c9ee20da5ac48d5efae81
Julien B <julien@toshokan.fr>
2019-04-27 16:26:57 +0200

    Adding second method to the API (GET -> Lambda getter)

--- terraform/Lab1-DynamoDB_Global_tables/api-gateway/api-gateway.tf
+++ terraform/Lab1-DynamoDB_Global_tables/api-gateway/api-gateway.tf
@@ -5,6 +5,7 @@ provider "aws" {
   region = "${var.region_provider}"
 }
 
+# API Declaration
 resource "aws_api_gateway_rest_api" "api-lambda-dynamodb" {
   provider    = "aws.region"
   name        = "lambda-dynamodb"
@@ -15,12 +16,16 @@ resource "aws_api_gateway_rest_api" "api-lambda-dynamodb" {
   }
 }
 
+# We create a resource called 'users'
+
 resource "aws_api_gateway_resource" "users" {
   provider    = "aws.region"
   rest_api_id = "${aws_api_gateway_rest_api.api-lambda-dynamodb.id}"
   parent_id   = "${aws_api_gateway_rest_api.api-lambda-dynamodb.root_resource_id}"
   path_part   = "users"
 }
+
+# Declaration of two methods for 'users' GET & POST
 
 resource "aws_api_gateway_method" "users" {
   provider      = "aws.region"
@@ -29,6 +34,16 @@ resource "aws_api_gateway_method" "users" {
   http_method   = "POST"
   authorization = "NONE"
 }
+
+resource "aws_api_gateway_method" "users_get" {
+  provider      = "aws.region"
+  rest_api_id   = "${aws_api_gateway_rest_api.api-lambda-dynamodb.id}"
+  resource_id   = "${aws_api_gateway_resource.users.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+# Lambda integration 
 
 resource "aws_api_gateway_integration" "lambda" {
   provider    = "aws.region"
@@ -41,6 +56,19 @@ resource "aws_api_gateway_integration" "lambda" {
   uri                     = "${var.lambda_filler_invoke}"
 }
 
+resource "aws_api_gateway_integration" "lambda_getter" {
+  provider    = "aws.region"
+  rest_api_id = "${aws_api_gateway_rest_api.api-lambda-dynamodb.id}"
+  resource_id = "${aws_api_gateway_method.users_get.resource_id}"
+  http_method = "${aws_api_gateway_method.users_get.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "${var.lambda_getter_invoke}"
+}
+
+# Deployment of this API
+
 resource "aws_api_gateway_deployment" "lambda" {
   provider    = "aws.region"
   depends_on  = ["aws_api_gateway_integration.lambda"]
@@ -48,15 +76,39 @@ resource "aws_api_gateway_deployment" "lambda" {
   stage_name  = "testing"
 }
 
+#######################################################
+# Adding permission for API Gateway to Lambda functions
+#######################################################
+
 resource "aws_lambda_permission" "apigw" {
   provider      = "aws.region"
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = "${var.lambda_name}"
+  function_name = "${var.lambda_filler_name}"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api-lambda-dynamodb.execution_arn}*/*"
+
+  # The /*/*/* part allows invocation from any stage, method and resource path
+  # within API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.api-lambda-dynamodb.execution_arn}*/*/*"
 }
 
+resource "aws_lambda_permission" "apigw_getter" {
+  provider      = "aws.region"
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${var.lambda_getter_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/*/* part allows invocation from any stage, method and resource path
+  # within API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.api-lambda-dynamodb.execution_arn}*/*/*"
+}
+
+##########################################
+# Methods responses & integration response
+##########################################
+
+# POST/Filler
 resource "aws_api_gateway_method_response" "200" {
   provider    = "aws.region"
   rest_api_id = "${aws_api_gateway_rest_api.api-lambda-dynamodb.id}"
@@ -67,9 +119,27 @@ resource "aws_api_gateway_method_response" "200" {
 
 resource "aws_api_gateway_integration_response" "IntegrationResponse" {
   provider    = "aws.region"
-  depends_on  = ["aws_api_gateway_integration.lambda"]
+  depends_on  = ["aws_api_gateway_integration.lambda_getter"]
   rest_api_id = "${aws_api_gateway_rest_api.api-lambda-dynamodb.id}"
   resource_id = "${aws_api_gateway_resource.users.id}"
   http_method = "${aws_api_gateway_method.users.http_method}"
   status_code = "${aws_api_gateway_method_response.200.status_code}"
+}
+
+# GET/Getter
+resource "aws_api_gateway_method_response" "200_getter" {
+  provider    = "aws.region"
+  rest_api_id = "${aws_api_gateway_rest_api.api-lambda-dynamodb.id}"
+  resource_id = "${aws_api_gateway_resource.users.id}"
+  http_method = "${aws_api_gateway_method.users_get.http_method}"
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "IntegrationResponseGetter" {
+  provider    = "aws.region"
+  depends_on  = ["aws_api_gateway_integration.lambda_getter"]
+  rest_api_id = "${aws_api_gateway_rest_api.api-lambda-dynamodb.id}"
+  resource_id = "${aws_api_gateway_resource.users.id}"
+  http_method = "${aws_api_gateway_method.users_get.http_method}"
+  status_code = "${aws_api_gateway_method_response.200_getter.status_code}"
 }
--- terraform/Lab1-DynamoDB_Global_tables/api-gateway/variables.tf
+++ terraform/Lab1-DynamoDB_Global_tables/api-gateway/variables.tf
@@ -4,10 +4,18 @@ variable "region_provider" {
   description = "Name of the provier to use for the creation of resources"
 }
 
-variable "lambda_name" {
+variable "lambda_filler_name" {
   description = "Name of the filler Lambda function"
 }
 
 variable "lambda_filler_invoke" {
   description = "Invoke ARN of the filler Lambda function"
+}
+
+variable "lambda_getter_name" {
+  description = "Name of the getter Lambda function"
+}
+
+variable "lambda_getter_invoke" {
+  description = "Invoke ARN of the getter Lambda function"
 }
