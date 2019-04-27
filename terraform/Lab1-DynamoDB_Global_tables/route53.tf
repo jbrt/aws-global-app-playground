7c39a615b845274680f94938b78fa4225672bc9a
Julien B <julien@toshokan.fr>
2019-04-27 17:14:05 +0200

    Adding Route53 resources records with weighted routing

---
+++ terraform/Lab1-DynamoDB_Global_tables/route53.tf
@@ -0,0 +1,39 @@
+# DNS - Route53
+# - creation on a RR api.<DOMAIN> with weighted policy
+
+data "aws_route53_zone" "lab" {
+  provider = "aws.primary"
+  name     = "${var.dns_domain}"
+}
+
+# Route53 on first region
+resource "aws_route53_record" "api-region1" {
+  provider = "aws.primary"
+  zone_id  = "${data.aws_route53_zone.lab.zone_id}"
+  name     = "api"
+  type     = "CNAME"
+  ttl      = "5"
+
+  weighted_routing_policy {
+    weight = 50
+  }
+
+  set_identifier = "region1"
+  records        = ["${module.api-gateway_region1.api-gateway_url}"]
+}
+
+# Route53 on second region
+resource "aws_route53_record" "api-region2" {
+  provider = "aws.primary"
+  zone_id  = "${data.aws_route53_zone.lab.zone_id}"
+  name     = "api"
+  type     = "CNAME"
+  ttl      = "5"
+
+  weighted_routing_policy {
+    weight = 50
+  }
+
+  set_identifier = "region2"
+  records        = ["${module.api-gateway_region2.api-gateway_url}"]
+}
